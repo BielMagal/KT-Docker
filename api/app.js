7d46e599dc4460a59f9c106927641f6aa5a3b846
Gabriel Magalhães <gabriel.magalhaes@dtidigital.com.br>
2020-07-30 01:12:22 -0300

    Hotfix api

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import mysql from 'mysql';
 import containerized from 'containerized';
